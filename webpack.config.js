b4482b3ad6553732585da53cd9661c6dd40a49c0
Rubel Ahmmed <ahmmedrubel.ad@gmail.com>
2022-05-23 21:31:16 +0600

    webapck configuration ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,15 @@
-const path = require('path');
+var path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
+    devServer: {
+        historyApiFallback: true,
+        static: 'dist'
+    },
+    mode: "development"
 }
